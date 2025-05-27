c2c12f7cd595372e41a01bc198301fee4fe3ffd3
Fatima Moumni <toumita.fatmoumni@gmail.com>
2025-05-27 22:05:22 +0100

    add upload page

---
+++ app/upload/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FileProcessingPipeline from "./components/FileProcessingPipeline";
+
+const page = () => {
+  return (
+    <div className="container mx-auto px-6 py-20">
+      <FileProcessingPipeline />
+    </div>
+  );
+};
+
+export default page;
