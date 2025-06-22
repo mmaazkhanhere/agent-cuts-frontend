f3c03c754de5f73f04a743e8347fa3a419e34b3f
Fatima Moumni <toumita.fatmoumni@gmail.com>
2025-06-22 23:53:45 +0100

    update style in about page

--- app/about/components/Mission.tsx
+++ app/about/components/Mission.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Mission = () => {
   return (
-     <div className="max-w-3xl mx-auto text-center py-20">
+     <div className="max-w-3xl mx-auto text-center pt-20">
         <h2 className="text-4xl md:text-5xl font-bold text-white mb-6 text-glow">
             <span className="bg-gradient-to-r from-teal-400 to-teal-600 bg-clip-text text-transparent">
               Our Mission
             </span>
           </h2>
-        <p className="text-lg text-gray-400  my-8">
+        <p className="text-lg text-gray-400 mt-8">
           At Agent Cuts, we are committed to making video editing more accessible
           and efficient through artificial intelligence. Our team combines
           expertise in AI, web development, and user experience to create a
